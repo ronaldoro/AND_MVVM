fe5f2927e65f7fa57e4b211dfff8947630ffdd42
wnsh3131@naver.com <wnsh3131@naver.com>
2021-07-14 22:54:48 +0900

    [Study MVVM in Android] Adapter Test

--- app/src/main/java/com/example/test_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/test_mvvm/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.test_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.test_mvvm.Adapter.ContactAdapter
 import com.example.test_mvvm.Model.Contact
 import com.example.test_mvvm.ViewModel.ContactViewModel
 
@@ -16,6 +20,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Set contactItemClick & contactItemLongClick lambda
+        val adapter = ContactAdapter({ contact ->
+            // put extras of contact info & start AddActivity
+        }, { contact ->
+            deleteDialog(contact)
+        })
+
+        val lm = LinearLayoutManager(this)
+        var main_recycleview = findViewById<RecyclerView>(R.id.main_recycleview)
+        main_recycleview.adapter = adapter
+        main_recycleview.layoutManager = lm
+        main_recycleview.setHasFixedSize(true)
+
+
         //Must be created with ViewModelProviders - If an already created ViewModel instance exists, it is returned.
         //contactViewModel = ViewModelProviders.of(this).get(ContactViewModel::class.java) -- deprecated
         contactViewModel = ViewModelProvider(this, ContactViewModel.Factory(application))
@@ -23,8 +41,21 @@ class MainActivity : AppCompatActivity() {
 
         //observe lifecycle of Activity or Fragment
         contactViewModel.getAll().observe(this, Observer<List<Contact>> { contacts ->
-            // Update UI
+            adapter.setContacts(contacts!!) // Update UI
         })
 
+        val contact = Contact(1, "name", "number", 'c')
+        contactViewModel.insert(contact)
+
+    }
+
+    private fun deleteDialog(contact: Contact) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Delete selected contact?")
+            .setNegativeButton("NO") { _, _ -> }
+            .setPositiveButton("YES") { _, _ ->
+                contactViewModel.delete(contact)
+            }
+        builder.show()
     }
 }
